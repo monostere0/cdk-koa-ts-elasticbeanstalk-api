7026d429f1c74b17a1b96dc4f0b5d765594cddda
Daniel Leu <leudanielm@gmail.com>
2020-04-04 19:52:06 +0200

    Adjust eslint for typescript

--- src/conf/index.ts
+++ src/conf/index.ts
@@ -1,25 +1,20 @@
 import packageJson from '../../package.json';
 
+interface EnvConfig {
+  GIT_COMMIT: string;
+  VERSION: string;
+  ENVIRONMENT: string;
+  AWS_REGION: string;
+}
+
 interface AppConfig {
-  port: number,
-  test_port: number,
-  cloudwatch_log_group: string,
-  cloudwatch_log_stream: string,
-  GIT_COMMIT: string,
-  VERSION: string,
-  ENVIRONMENT: string,
-  AWS_REGION: string,
+  port: number;
+  test_port: number;
+  cloudwatch_log_group: string;
+  cloudwatch_log_stream: string;
 };
 
-function conf(): AppConfig {
-  const env = process.env.NODE_ENV;
-  const config = require(`./${(env || 'default')}.json`);
-  const defaultConfig = env ? require('./default.json') : {};
-
-  return Object.assign({}, defaultConfig, config, getEnvConfig());
-};
-
-function getEnvConfig() {
+function getEnvConfig(): EnvConfig {
   return {
     GIT_COMMIT: process.env.CIRCLE_SHA1,
     VERSION: packageJson.version,
@@ -27,5 +22,13 @@ function getEnvConfig() {
     AWS_REGION: process.env.AWS_REGION,
   };
 }
+
+function conf(): AppConfig & EnvConfig {
+  const env = process.env.NODE_ENV;
+  const config = require(`./${(env || 'default')}.json`);
+  const defaultConfig = env ? require('./default.json') : {};
+
+  return Object.assign({}, defaultConfig, config, getEnvConfig());
+};
 
 export default conf;
