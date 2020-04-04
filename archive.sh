5f80614999d3ec2175af8f3e80630bc6eafbf1be
Daniel Leu <leudanielm@gmail.com>
2020-04-04 20:25:23 +0200

    Update the building and archiving process

--- archive.sh
+++ archive.sh
@@ -1,4 +1,20 @@
-rm -rf dist
+# export the random zip name
 export BUNDLE_NAME=$(openssl rand -hex 12)
+
+# clean the previous builds
+rm -rf dist
+
+# build the source code
+npm run build
+
+# create the dist folder
 mkdir -p dist
-git archive -o "dist/$BUNDLE_NAME.zip" --format=zip HEAD
+
+# archive only the files in the build dir, not its contents
+cd build; zip -rq "../dist/$BUNDLE_NAME.zip" *
+
+# go back to root
+cd ..
+
+# clean the build output
+rm -rf build
