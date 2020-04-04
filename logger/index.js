79b3130e1c6ef5325360c46cd938221c921db896
Daniel Leu <leudanielm@gmail.com>
2020-04-04 16:48:40 +0200

    Enable CW logger for prod only

--- logger/index.js
+++ logger/index.js
@@ -1,23 +1,24 @@
 const bunyan = require('bunyan');
 const conf = require('../conf')();
-const createCloudWatchStream = require('bunyan-cloudwatch');
-
 const packageJson = require('../package.json');
 
-const cloudWatchStream = createCloudWatchStream({
-  logGroupName: conf.cloudwatch_log_group,
-  logStreamName: conf.cloudwatch_log_stream,
-  cloudWatchLogsOptions: {
-    region: process.env.AWS_REGION,
-  },
-});
+const logStreams = [];
+
+if (process.env.NODE_ENV === 'prod') {
+  const cloudWatchStream = require('bunyan-cloudwatch')({
+    logGroupName: conf.cloudwatch_log_group,
+    logStreamName: conf.cloudwatch_log_stream,
+    cloudWatchLogsOptions: {
+      region: process.env.AWS_REGION,
+    },
+  });
+
+  logStreams.push({
+    type: 'raw',
+    stream: cloudWatchStream,
+  });
+}
 
 module.exports = bunyan.createLogger({
   name: packageJson.name,
-  streams: [
-    {
-      type: 'raw',
-      stream: cloudWatchStream,
-    },
-  ],
 });
